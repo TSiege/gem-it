2e06c1271f7edf69ad6d1693c0b0229f53964d27
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-04-09 15:39:40 -0400

    sets up sessions with token from github

--- app/controllers/gemifier_controller.rb
+++ app/controllers/gemifier_controller.rb
@@ -1,9 +1,15 @@
 class GemifierController < ApplicationController
   def index
-
+    render "omniauth_success" if session[:token]
   end
 
   def create
-    raise env["omniauth.auth"]
+    @client = Octokit::Client.new(:access_token => session[:token])
+    @client.create_repo(params["repo_name"])
+  end
+
+  def omniauth
+    session[:token] = env["omniauth.auth"]["credentials"]["token"]
+    redirect_to "/"
   end
 end
