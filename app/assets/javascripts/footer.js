60593fdff686fceb10730105ddd68281e39545d1
georgemayer <mayer.georgep@gmail.com>
2014-04-10 17:12:48 -0400

    window panels opening

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -1,5 +1,16 @@
 function reveal() {
 
-  $("#foot").addClass("footer-down")
+  $("#foot").addClass("footer-down");
+  $("#head").addClass("header-up");
+
+  setTimeout(function() {
+    $("#frame-bg").css({
+      "position": "static",
+      "transform": "translateY(-28px)"
+    });
+    $("#head").hide();
+    $("#foot").removeClass("footer-down");
+
+  }, 1300);
 
 }
