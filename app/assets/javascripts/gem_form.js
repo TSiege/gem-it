93c5b41347296be6427db18762f5d5ea61570792
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-04-11 11:38:46 -0400

    succesfully adds new fields on js call

--- app/assets/javascripts/gem_form.js
+++ app/assets/javascripts/gem_form.js
@@ -1,10 +1,23 @@
 $( document ).ready(function() {
   $(".glyphicon-plus").on("click", function(e){
     var glyphicon = $(this)
-    var newFormField = $("<div>")
-    newFormField.attr()
-    
+    var newFormDiv = $("<div>")
+    newFormDiv.attr("id", "last-gem-method");
+
+    var newFormField = '<label for="method-name-1">Method Name</label>' +
+      '<input class="form-control" id="last-method-field" name="method-name-1" type="text" value=""><br>' +
+      '<label for="method-data-1">Is this the data you want?</label>' +
+      '<p><span id="last-data-field"></span></p>' +
+      '<input id="last-path-field" name="last-path-field" type="hidden" value="">';
+
+    newFormDiv.append(newFormField);
+
+    glyphicon.parent().removeAttr();
     glyphicon.siblings("input").attr("disabled", "disabled");
-    glyphicon.prepend(newFormField);
+    glyphicon.prepend(newFormDiv);
   });
 });
+
+
+
+
