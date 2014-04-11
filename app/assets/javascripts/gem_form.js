16f6cdcc63cb2d0068d1aed52117121e298bb14a
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-04-11 14:47:09 -0400

    toggles between tabs

--- app/assets/javascripts/gem_form.js
+++ app/assets/javascripts/gem_form.js
@@ -1,7 +1,24 @@
 $( document ).ready(function() {
   switchFormTabs();
   addFormFields();
+  switchToGemIt();
+  switchToGetIt();
 });
+
+function switchToGemIt() {
+  $(".switch-to-gem-it").click(function(){
+    $("ul.list-inline li").toggleClass("active");
+    $("#gem-info").show();
+    $("#gem-methods").hide();
+  });
+}
+
+function switchToGetIt() {
+  $(".switch-to-get-it").click(function(){
+    $("#gem-info").hide();
+    $("#gem-methods").show();
+  });
+}
 
 function switchFormTabs() {
     $("ul.list-inline li").click(function(){
