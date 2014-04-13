5354f4d9d61d3843eb39b06a2463949db29e995f
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-04-13 13:54:56 -0400

    adds validations and warning messages for method name length, non-existant data, and begins method name uniqueness

---
+++ app/assets/javascripts/validations.js
@@ -0,0 +1,10 @@
+// $( document ).ready(function() {
+
+//   var regEx = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/
+
+//   // var $allErrors = $("span#user_age_error, span#user_ph_error, span#user_email_error");
+
+//   // $allErrors.hide();
+
+
+// });
