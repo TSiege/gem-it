d83b8f4824bbba06edae872303062756fb1acce5
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-04-21 16:08:39 -0400

    sets up method to weed out browser styles

--- app/assets/javascripts/css_computed_style_selector.js
+++ app/assets/javascripts/css_computed_style_selector.js
@@ -1,3 +1,41 @@
 $( document ).ready(function() {
   
 });
+
+
+function selectElementsStyles() {
+  $("iframe").contents().find("html").each(function() {
+    $(this).bind("click", function(e) { 
+      e.preventDefault()
+      var selectedElementStyles = $(e.target).getStyles(),
+          defaultElementStyles = $("<" + e.target.tagName + ">").getStyles(),
+          foundStyles = selectUniqueStylesFrom(selectedElementStyles, defaultElementStyles);
+      console.log(selectedElementStyles)
+      console.log(defaultElementStyles)
+      console.log(foundStyles)
+      printStylesOf(foundStyles);
+    });
+  });
+}
+
+function selectUniqueStylesFrom(targetElement, defaultElement) {
+  var uniqueProperties = {};
+
+  for (var prop in targetElement) {
+    console.log("target Element: " + targetElement[prop]);
+    console.log("default Element: " + defaultElement[prop]);
+    if (targetElement[prop] !== defaultElement[prop] &&
+      defaultElement[prop] !== "") {
+      uniqueProperties[prop] = targetElement[prop];
+    }
+  }
+  return uniqueProperties;
+}
+
+function printStylesOf(uniqueStyleObject){
+  var styleString = "";
+  for (var prop in uniqueStyleObject) {
+    styleString += ("" + prop + ": " + uniqueStyleObject[prop] + ";\n");
+  }
+  $("p.lead.computed-css").text(styleString);
+}
